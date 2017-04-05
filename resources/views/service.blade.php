b99bf17e1a1bb3e43f92b02537da84857e5c6a55
kongyanzhou <yanzhou@microdreams.com>
2017-04-06 01:11:28 +0800

    提交diamante

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -44,17 +44,35 @@
 				<div class="seperate"></div>
 				<div class="services">
 					<ul>
-						<li>客服一：503616985 <a href=""><img src="images/qq.png" alt="">k</a> 处理范围：三星刷机</li>
-						<li>客服一：503616985 <img src="" alt=""> 处理范围：三星刷机</li>
-						<li>客服一：503616985 <img src="" alt=""> 处理范围：三星刷机</li>
-						<li>客服一：503616985 <img src="" alt=""> 处理范围：三星刷机</li>
-						<li>客服一：503616985 <img src="" alt=""> 处理范围：三星刷机</li>
+						<li>客服一：<span>503616985</span> <a href="tencent://message/?uin=客服一：503616985&Site=&Menu=yes"><img src="images/qq.png" alt=""></a> 处理范围：<em>三星刷机</em></li>
+						<li>客服二：<span>503616985</span> <a href="tencent://message/?uin=客服一：503616985&Site=&Menu=yes"><img src="images/qq.png" alt=""></a> 处理范围：<em>三星刷机</em></li>
+						<li>客服三：<span>503616985</span> <a href="tencent://message/?uin=客服一：503616985&Site=&Menu=yes"><img src="images/qq.png" alt=""></a> 处理范围：<em>三星刷机</em></li>
+						<li>客服四：<span>503616985</span> <a href="tencent://message/?uin=客服一：503616985&Site=&Menu=yes"><img src="images/qq.png" alt=""></a> 处理范围：<em>三星刷机</em></li>
 					</ul>
 				</div>
 			</div>
 				
-			<div class="contact">
-				
+			<div class="contact clearfix">
+				<p>客服QQ：123456789 （工作时间段：10：00 -- 22：00）</p>
+				<p class="tips">请务必先查看下面的处理范围及常见问题</p>
+				<div class="layout-left">
+					<h4>客服处理范围</h4>
+					<ul>
+						<li>1：充值及退款问题。</li>
+						<li>2：ROM文件添加（请自行先确认您要添加的固件或型号在本站型号列表有收录）</li>
+						<li>3：网站故障方面。</li>
+						<li>4：不提供手机故障处理，远程刷机等一切非下载问题。</li>
+					</ul>
+				</div>
+				<div class="line"></div>
+				<div class="laytout-right">
+					<h4>常见问题</h4>
+					<ul>
+						<li>1：<em>解压密码是多少？</em>在文件购买页面有明显注明，请自行查看，不必添加QQ。</li>
+						<li>2：<em>付款后多久到账？</em>在支付宝或QQ付款，只要付款时有填写备注，10分内到账，期间请多次刷新网页查看流量是否到账，长时间未到才联系。</li>
+						<li>3：<em>如果是支付宝或QQ付款未填写备注用户UID？</em>添加客服后，联系时请提供您的支付宝账号或者QQ账号和用户UID。</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 

9eb50cb956b2c22f46bc6ba9ebdf95a4a03b954e
kongyanzhou <yanzhou@microdreams.com>
2017-04-15 23:04:06 +0800

    提交代码

---
+++ public/scripts/rom.js
@@ -0,0 +1,22 @@
+
+
+(function($) {
+
+
+	// 实例化vue
+	var vm = new Vue({
+	    el: '#app',
+	    data: {
+
+	    },
+	    methods: {
+	    },
+	});
+
+    
+    $(function() {
+
+
+    });
+
+})(jQuery)
--- resources/views/rom.blade.php
+++ resources/views/rom.blade.php
@@ -1,4 +1,4 @@
-a<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="zh-cmn-Hans">
 <head>
 	<meta charset="utf-8">
@@ -9,9 +9,9 @@ a<!DOCTYPE html>
 	<meta name="viewport" contant="width=device-width, initial-scale=1">
 	<meta name="keywords" content="">
 	<meta name="description" content="">
-
     <link href="//cdn.bootcss.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href="style/css/load.css" rel="stylesheet">
+	 <link rel="stylesheet" href="http://www.jq22.com/jquery/bootstrap-3.3.4.css">
+    <link href="style/css/rom.css" rel="stylesheet">
 </head>
 <body>
 	<div id="app" data-value="{{ csrf_token() }}">
@@ -42,15 +42,147 @@ a<!DOCTYPE html>
 			</div>
 		</nav>
 
-		
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-9">
+					<div class="panel panel-default panel-notice form">
+						<div class="panel-heading">
+							<div class="panel-title">
+								<div class="breadcrumb">
+									<ol>
+										<li><a href="javascript:history.back()">返回</a></li>
+										<li class="active">添加ROM</li>
+									</ol>
+								</div>
+							</div>
+						</div>
 
+						<div class="panel-body">
+							<form class="form-horizontal" role="form">
+								<div class="form-group">
+								  	<label for="basic" class="col-sm-2 control-label">品牌</label>
+								  	<div class="col-sm-4">
+								    	<select id="basic" class="selectpicker show-tick form-control" data-live-search="true">
+									    	<option disabled selected style='display:none;'>选择一个品牌</option>  
+											<option value="1">一星大棒子</option>
+											<option value="2">二星大棒子</option>
+											<option value="3">三星大棒子</option>
+											<option value="4">四星大棒子</option>
+											<option value="1">一星大棒子</option>
+											<option value="2">二星大棒子</option>
+											<option value="3">三星大棒子</option>
+											<option value="4">四星大棒子</option>
+											<option value="1">一星大棒子</option>
+											<option value="2">二星大棒子</option>
+											<option value="3">三星大棒子</option>
+											<option value="4">四星大棒子</option>
+											<option value="1">一星大棒子</option>
+											<option value="2">二星大棒子</option>
+											<option value="3">三星大棒子</option>
+											<option value="4">四星大棒子</option>
 
+								    	</select>
+								  	</div>
 
+								  	<label for="basic" class="col-sm-2 control-label">机型</label>
+								  	<div class="col-sm-4">
+								    	<select id="basic" class="selectpicker show-tick form-control" data-live-search="true">
+									    	<option disabled selected style='display:none;'>选择一个机型</option>  
+											<option value="1">一星大棒子</option>
+											<option value="2">二星大棒子</option>
+											<option value="3">三星大棒子</option>
+											<option value="4">四星大棒子</option>
+								    	</select>
+								  	</div>
+								</div>
 
+								<div class="form-group">
+								  	<label for="basic" class="col-sm-2 control-label">国家</label>
+								  	<div class="col-sm-4">
+								    	<select id="basic" class="selectpicker show-tick form-control" data-live-search="true">
+									    	<option disabled selected style='display:none;'>选择一个国家</option>  
+											<option value="1">一星大棒子</option>
+											<option value="2">二星大棒子</option>
+											<option value="3">三星大棒子</option>
+											<option value="4">四星大棒子</option>
+								    	</select>
+								  	</div>
 
+								  	<label for="basic" class="col-sm-2 control-label">OS</label>
+								  	<div class="col-sm-4">
+								    	<select id="basic" class="selectpicker show-tick form-control" data-live-search="true">
+									    	<option disabled selected style='display:none;'>选择一个版本</option>  
+											<option value="1">一星大棒子</option>
+											<option value="2">二星大棒子</option>
+											<option value="3">三星大棒子</option>
+											<option value="4">四星大棒子</option>
+								    	</select>
+								  	</div>
+								</div>
 
+								<div class="form-group">
+								  	<label for="basic" class="col-sm-2 control-label">类型</label>
+								  	<div class="col-sm-4">
+								    	<select id="basic" class="selectpicker show-tick form-control" data-live-search="true">
+									    	<option disabled selected style='display:none;'>选择一个类型</option>  
+											<option value="1">一星大棒子</option>
+											<option value="2">二星大棒子</option>
+											<option value="3">三星大棒子</option>
+											<option value="4">四星大棒子</option>
+								    	</select>
+								  	</div>
 
-		
+								  	 <label for="qq" class="col-sm-2 control-label">单价(金币)</label>
+						            <div class="col-sm-4">
+						                <input type="text" class="form-control" id="qq" value="" placeholder="请输入价格">
+						            </div>
+								</div>
+
+								<div class="form-group">
+						            <label for="qq" class="col-sm-2 control-label">版本</label>
+						            <div class="col-sm-10">
+						                <input type="text" class="form-control" id="qq" value="" placeholder="请输入版本号">
+						            </div>
+						           
+						        </div>
+
+						        <div class="form-group">
+
+						        	<label for="qq" class="col-sm-2 control-label">下载地址</label>
+						            <div class="col-sm-10">
+						                <input type="text" class="form-control" id="qq" value="" placeholder="资源下载地址">
+							        </div>
+						        </div>
+
+						        <div class="form-group">
+						            <label for="qq" class="col-sm-2 control-label">备注</label>
+						            <div class="col-sm-10">
+						                <input type="text" class="form-control" id="qq" value="" placeholder="下载密码/解压密码/注释框等">
+						            </div>
+						        </div>
+
+								 <div class="form-group buttonRow">
+						            <div class="col-sm-offset-2 col-sm-10">
+						                <button type="submit" class="btn btn-info btn-block"><i class="fa fa-pencil"></i> &nbsp;提交</button>
+						            </div>
+						        </div>
+							</form>				
+						</div>
+					</div>				
+				</div>
+
+				<div class="col-sm-3">
+					<div class="panel panel-default">
+				        <div class="panel-heading">
+				            <div class="panel-title">上传须知</div>
+				        </div>
+				        <div class="panel-body">
+				            <p>您可以自由上传手上拥有的资源，并设定合适的价格。一旦审核通过并有会员购买下载，我们将给您的账户充值您设定价格的10%的金币。例如：您上传的ROM的价格为50金币。则，每次一旦有会员购买下载，我们都将给您的账户充值5个金币。</p>
+				        </div>
+				    </div>
+				</div>
+			</div>
+		</div>
 
 		<div class="footer">
 			<p>粤ICP备17024526号-1</p>
@@ -59,10 +191,13 @@ a<!DOCTYPE html>
 
 	
 	<script src="scripts/lib/jquery/jquery.min.js"></script>
+	<script src="scripts/lib/bootstrap/bootstrap.min.js"></script>
+	<script src="scripts/lib/bootstrap/bootstrap-select.min.js"></script>
 	<script src="scripts/lib/vue/vue.min.js"></script>
-	<script src="scripts/load.js"></script>
+	<script src="scripts/rom.js"></script>
 </body>
 </html>
+
 
 
 

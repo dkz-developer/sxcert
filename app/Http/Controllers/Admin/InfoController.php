d887a936d0c9b4606aa0c273048e84c511ea665c
xuanzhou@microdreams.com <xuanzhou@microdreams.com>
2017-04-21 01:11:33 +0800

    删除接口

--- app/Http/Controllers/Admin/InfoController.php
+++ app/Http/Controllers/Admin/InfoController.php
@@ -165,17 +165,17 @@ class InfoController extends Controller
 	public function common()
 	{
 		$Model = new InfoCommon();
-		$brand = $Model->where('type',1)->paginate(10);
+		$brand = $Model->where('type',1)->orderBy('id', 'desc')->paginate(10);
 		$brandCount = $Model->where('type',1)->count();
-		$model = $Model->where('type',2)->paginate(10);
+		$model = $Model->where('type',2)->orderBy('id', 'desc')->paginate(10);
 		$modelCount = $Model->where('type',2)->count();
-		$country = $Model->where('type',3)->paginate(10);
+		$country = $Model->where('type',3)->orderBy('id', 'desc')->paginate(10);
 		$countryCount = $Model->where('type',3)->count();
-		$os = $Model->where('type',4)->paginate(10);
+		$os = $Model->where('type',4)->orderBy('id', 'desc')->paginate(10);
 		$osCount = $Model->where('type',4)->count();
-		$type = $Model->where('type',5)->paginate(10);
+		$type = $Model->where('type',5)->orderBy('id', 'desc')->paginate(10);
 		$typeCount = $Model->where('type',5)->count();
-		$tag = $Model->where('type',6)->paginate(10);
+		$tag = $Model->where('type',6)->orderBy('id', 'desc')->paginate(10);
 		$tagCount = $Model->where('type',6)->count();
 		$result = [
 			'brand'=>$brand,
@@ -207,5 +207,22 @@ class InfoController extends Controller
 		if($result)
 			return response()->json(['code'=>'S','msg'=>'添加成功']);
 		return response()->json(['code'=>'F','msg'=>'添加失败']);
+	}
+	
+	
+	public function commonDelete(Request $request)
+	{
+	    $type = $request->input('type');
+	    $id = $request->input('id');
+	    
+	    if(! $type || ! $id)
+	        return response()->json(['code'=>'F','msg'=>'参数错误']);
+	    
+	    $Model = new InfoCommon();
+	    
+	    $result = $Model->where('type',$type)->where('id',$id)->delete();
+	    if($result)
+	        return response()->json(['code'=>'S','msg'=>'操作成功']);
+	    return response()->json(['code'=>'F','msg'=>'操作失败']);
 	}
 }

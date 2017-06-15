6fd9f0d30039adb72aa904f3b7f597b34d47a3ae
kongyanzhou <yanzhou@microdreams.com>
2017-06-16 00:54:11 +0800

    开发详情页

--- public/scripts/thread.js
+++ public/scripts/thread.js
@@ -2,28 +2,8 @@
 
 (function($) {
 
-	//底部轮播图
-    var bottombannerEvent = function() {
-        $('.flickadd').flicker({dot_navigation: true});
-    };
-
-	// 实例化vue
-	var vm = new Vue({
-	    el: '#app',
-	    data: {
-	    	
-	    },
-	    methods: {
-	        
-	    }
-	});
-
-
-
     $(function() {
-
-	    bottombannerEvent();
-
+    	var ue = UE.getEditor('pubContent');
     });
 
 })(jQuery)
